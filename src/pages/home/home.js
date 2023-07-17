4f13397a735a3219fad24d6e6cec12dab5521128
Ali KOÇAN <alikcn35@gmail.com>
2023-07-17 20:08:44 +0300

    'redux'

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import Counter from '../../components/Counter'
-import Theme from '../../components/Theme'
-import { Container } from 'react-bootstrap'
-import { useSelector } from 'react-redux'
-import Content from '../../components/Content'
-import Localization from '../../components/Localization'
+import React from "react";
+import Counter from "../../components/Counter";
+import Theme from "../../components/Theme";
+import { Container } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import Content from "../../components/Content";
+import Localization from "../../components/Localization";
 
 const Home = () => {
-  const themeState = useSelector((state) => state.theme)
+  const themeState = useSelector((state) => state.theme);
 
-  const theme = themeState.theme
+  const theme = themeState.theme;
 
-  const oppositeTheme = theme === "light" ? "dark" : "light"
+  const oppositeTheme = theme === "light" ? "dark" : "light";
 
   return (
     <Container fluid className={`bg-${theme} text-${oppositeTheme} h-100 p-3`}>
@@ -19,8 +19,14 @@ const Home = () => {
       <Counter />
       <Localization />
       <Content />
+      <div>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni ad
+        cupiditate, nulla neque optio commodi id ipsa iste doloribus. Officiis
+        tempora sit consequuntur commodi eligendi veritatis, ipsam molestiae
+        animi quos?
+      </div>
     </Container>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
